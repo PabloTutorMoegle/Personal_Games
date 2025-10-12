7d11e50671d2566b889d16bf12a51b1f51fb208e
PabloTutorMoegle <pablotutormoegle@gmail.com>
2025-10-12 11:51:56 +0200

    updates

---
+++ AdventureRemake/Adventure/Assets/Scripts/Controllers/CastleController.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class CastleController : MonoBehaviour
+{
+    public static event System.Action GameFinished;
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            var playerInventory = collision.GetComponent<PlayerInventorySystem>();
+            bool hasItems = false;
+            if (playerInventory != null)
+            {
+                hasItems = playerInventory.HasItem("Key") && playerInventory.HasItem("Chest");
+            }
+            if (hasItems)
+            {
+                Debug.Log("Player has reached the castle with required items. Game Finished!");
+                GameFinished?.Invoke();
+            }
+        }
+    }
+}
